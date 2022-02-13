6ad25fe7026441178548f26eec9092356f9fe3e2
神雕 <teasiu@qq.com>
2022-02-13 11:48:09 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -28,15 +28,54 @@ cat << EOF > /etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "$UUID",
-                        "level": 0
+                        "id": "$UUID", // 填写你的 UUID
+                        "level": 0,
+                        "email": "love@v2fly.org"
                     }
                 ],
-                 "decryption": "none"
+                "decryption": "none",
+                "fallbacks": [
+                    {
+                        "dest": 80
+                    },
+                    {
+                        "path": "/slitaz", // 必须换成自定义的 PATH
+                        "dest": 1234,
+                        "xver": 1
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "tls",
+                "tlsSettings": {
+                    "alpn": [
+                        "http/1.1"
+                    ],
+                }
+            }
+        },
+        {
+            "port": 1234,
+            "listen": "127.0.0.1",
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID", // 填写你的 UUID
+                        "level": 0,
+                        "email": "love@v2fly.org"
+                    }
+                ],
+                "decryption": "none"
             },
             "streamSettings": {
                 "network": "ws",
-                "security": "none"
+                "security": "none",
+                "wsSettings": {
+                    "acceptProxyProtocol": true, // 提醒：若你用 Nginx/Caddy 等反代 WS，需要删掉这行
+                    "path": "/slitaz" 
+                }
             }
         }
     ],
